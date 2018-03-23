4974b62ef9d9101e632c61d4da64b5ff74f41a61
mariagnzalez <37300174+mariagnzalez@users.noreply.github.com>
2018-03-23 12:32:48 +0100

    Update programa-1.py

--- openfd-1/programa-1.py
+++ openfd-1/programa-1.py
@@ -3,15 +3,15 @@ import json
 
 headers = {'User-Agent': 'http-client'}
 
-connection = http.client.HTTPSConnection("api.fda.gov")#estabelecemos conexión con esa pagina
-connection.request("GET", "/drug/label.json", None, headers)# le pedimos que nos de la informacion peidida
+connection = http.client.HTTPSConnection("api.fda.gov")#establecemos conexión con esa pagina
+connection.request("GET", "/drug/label.json", None, headers)# le pedimos que nos de la información pedida
 r1 = connection.getresponse()
 print(r1.status, r1.reason)#imprimimos el estado:200 y la razon:OK
 repos_raw = r1.read().decode("utf-8")
 connection.close()
 
 
-repos = json.loads(repos_raw)#transformamos dicho docuemnto en diccionario, listas para que asi podamos acceder de una forma mas sencilla
+repos = json.loads(repos_raw)#transformamos dicho docuemento en diccionario, listas para que asi podamos acceder de una forma mas sencilla
 info_medicamento=repos['results'][0]
 
 print ('- Identificador del medicamento: ',info_medicamento['id'])
