d5c129221bc16d90c8e5c653f82c9c1bfd574625
mariagnzalez <37300174+mariagnzalez@users.noreply.github.com>
2018-03-29 11:38:58 +0200

    Update programa-2.py

--- openfda-1/programa-2.py
+++ openfda-1/programa-2.py
@@ -12,7 +12,7 @@ connection.close()
 repos= json.loads(repos_raw)
 #para acceder a los 10 identificadores hago uso de un bucle for.
 for i in range (len (repos['results'])):#recorro la lista de 'results' (teniendo 10 posiciones)
-    info_medicamento=repos['results'][i]#obtedremos la información de cada medicamento ya que la 'i' tomará valores de 0-9
+    info_medicamento=repos['results'][i]#obtendremos la información de cada medicamento ya que la 'i' tomará valores de 0-9
 
     print ('- Identificador del medicamento ',i+1,': ' ,info_medicamento['id'])
 #La modificación se establece en la forma que tenemos que pedir a la página. En este caso imponemos un límite como dice la propia pagina que se debe de hacer
